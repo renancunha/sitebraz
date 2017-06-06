bce3ea66615c98e89d81d9f11de5cdc0d716ea30
Renan Cunha <renan.bara@hotmail.com>
2017-06-06 09:44:23 -0300

    mudancas

--- server/config/environment/index.js
+++ server/config/environment/index.js
@@ -3,7 +3,7 @@
 var path = require('path');
 var _ = require('lodash');
 
-process.env.NODE_ENV = 'production';
+//process.env.NODE_ENV = 'production';
 
 function requiredProcessEnv(name) {
   if (!process.env[name]) {
